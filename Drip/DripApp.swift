8b2f9a27180fe5904c251c0e13e7729117bc9dd7
Todd Hamilton <180131+toddham@users.noreply.github.com>
2022-03-13 15:53:32 -0400

    Initial commit

---
+++ Drip/DripApp.swift
@@ -0,0 +1,17 @@
+//
+//  DripApp.swift
+//  Drip
+//
+//  Created by Todd Hamilton on 8/12/21.
+//
+
+import SwiftUI
+
+@main
+struct DripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
